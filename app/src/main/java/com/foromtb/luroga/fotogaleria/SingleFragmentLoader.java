a42b6768d2fe9532a96cb13bc19435de3ce18686
lromeral <luis.romeral@gmail.com>
2017-07-10 11:48:45 +0200

    SingleFragment

--- app/src/main/java/com/foromtb/luroga/fotogaleria/SingleFragmentLoader.java
+++ app/src/main/java/com/foromtb/luroga/fotogaleria/SingleFragmentLoader.java
@@ -4,21 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 
 /**
  * Created by LuisR on 10/07/2017.
  */
 
-public abstract class SingleFragmentLoader extends Fragment {
+public abstract class SingleFragmentLoader extends AppCompatActivity {
     protected abstract Fragment createFragment();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FragmentManager fragmentManager = getFragmentManager();
-        Fragment fg = createFragment();
+        setContentView(R.layout.single_fragment_loader);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fg = fragmentManager.findFragmentById(R.id.fragment_container);
         if (fg == null){
+            fg = createFragment();
             fragmentManager.beginTransaction().add(R.id.fragment_container,fg).commit();
         }
 
